7a3a02c053af0a9213c331211831cae990be2213
Fred Feng <fredfeng@umich.edu>
2024-01-06 01:32:09 -0500

    Minor changes.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,7 +29,7 @@ templates_path = ['_templates']
 
 # -- Options for HTML output
 
-html_theme = 'sphinx_rtd_theme'
+# html_theme = 'sphinx_rtd_theme'
 # html_static_path = ['_static']
 # html_logo = "imse440-logo.png"
 html_theme_options = {
